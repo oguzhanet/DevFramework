a4cf677044fc87a900a61965237f542a254a946e
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-04-26 21:22:24 +0300

    Add project files.

---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHerper.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using DevFramework.Core.DataAccess.NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers
+{
+    public class SqlServerHerper:NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012
+                    .ConnectionString(c => c.FromConnectionStringWithKey("NorthwindContext")))
+                .Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+                .BuildSessionFactory();
+        }
+    }
+}
